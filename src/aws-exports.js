2fad4c5016820014a1faea2bf0aa71e2b3fdc8f4
ExpandingDev <tylersengia@gmail.com>
2023-03-12 13:49:08 -0400

    Adding AWS configuration that reads in from .env file

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    "aws_appsync_graphqlEndpoint": process.env.REACT_APP_AWS_GRAPHQLENDPOINT,
+    "aws_appsync_region": process.env.REACT_APP_AWS_REGION,
+    "aws_appsync_authenticationType": process.env.REACT_APP_AWS_AUTHENTICATIONTYPE,
+    "aws_appsync_apiKey": process.env.REACT_APP_AWS_APIKEY,
+    };
+    
+    export default awsmobile;
