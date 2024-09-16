acc6a252fefbc2e2f953a5002af22463df5374ed
Tyler Sengia <tylersengia@gmail.com>
2024-09-16 14:05:32 -0400

    fix typo in env var name

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,6 +1,6 @@
 const aws_settings = {
     aws_appsync_graphqlEndpoint: process.env.REACT_APP_AWS_GRAPHQLENDPOINT,
-    aws_appsync_realtimeEndpoint: process.env.REACT_aPP_AWS_REALTIMEENDPOINT,
+    aws_appsync_realtimeEndpoint: process.env.REACT_APP_AWS_REALTIMEENDPOINT,
     aws_appsync_region: process.env.REACT_APP_AWS_REGION,
     aws_appsync_authenticationType: process.env.REACT_APP_AWS_AUTHENTICATIONTYPE,
     aws_appsync_apiKey: process.env.REACT_APP_AWS_APIKEY,
