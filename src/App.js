7743dbbba29f084015d2ae890a8f9edcd94997c6
Tyler Sengia <tylersengia@gmail.com>
2024-09-16 15:33:54 -0400

    remove dead code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
 import DashboardComponent from "./Dashboard.js";
-import aws_settings from "./aws-exports.js"
 
 function App() {
   return (
-     <DashboardComponent defaultAPI={aws_settings.aws_appsync_graphqlEndpoint} />
+     <DashboardComponent />
   );
 }
 
