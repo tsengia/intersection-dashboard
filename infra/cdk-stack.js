61e4afd3933f5c16bb75b6a221592046e3c1bfb1
Tyler Sengia <tylersengia@gmail.com>
2024-09-19 18:28:47 -0400

    adding cdk to dependencies

---
+++ infra/cdk-stack.js
@@ -0,0 +1,22 @@
+import { Stack, Duration } from "aws-cdk-lib";
+
+class CdkStack extends Stack {
+  /**
+   *
+   * @param {Construct} scope
+   * @param {string} id
+   * @param {StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+  }
+}
+
+export { CdkStack };
