570279bbf1ac82ec636a5540cf77f59cf6863398
Tyler Sengia <tylersengia@gmail.com>
2024-09-21 17:28:56 -0400

    grab current git branch using simple git

--- infra/cdk.js
+++ infra/cdk.js
@@ -4,5 +4,12 @@ import { App } from "aws-cdk-lib";
 
 import { CdkStack } from "./cdk-stack.js";
 
+import { simpleGit } from 'simple-git';
+
+
 const app = new App();
-new CdkStack(app, 'CdkStack', {});
+let amplifyBranchName = await simpleGit().revparse(['--abbrev-ref', 'HEAD']);
+new CdkStack(app, 'CdkStack', {
+    amplifyAppId: process.env.AMPLIFY_APP_ID,
+    amplifyBranchName: amplifyBranchName
+});
